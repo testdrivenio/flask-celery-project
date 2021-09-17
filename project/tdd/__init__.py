b27090bf90ec05e248d23ec586aaa5064c4e89bc
Michaelyin <hgyinjian@gmail.com>
2021-09-17 13:36:32 +0800

    remove old file

--- project/tdd/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-tdd_blueprint = Blueprint('tdd', __name__, url_prefix='/tdd', template_folder='templates')
-
-from . import models, views  # noqa
--- project/users/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-users_blueprint = Blueprint("users", __name__, url_prefix="/users", template_folder="templates")
-
-from . import events, models, tasks, views  # noqa
